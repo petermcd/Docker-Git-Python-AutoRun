a8345b6f0d0203025a1b3b9bd26d7a492f0309ae
Peter McDonald <me@petermcdonald.co.uk>
2022-09-20 20:59:29 +0100

    Updated to produce a multi arch images

--- make.bat
+++ make.bat
@@ -1,5 +1,16 @@
-docker build --tag petermcd/python_autorun:0.0.1 .
-docker tag petermcd/python_autorun:0.0.1 petermcd/python_autorun:latest
+@ECHO OFF
+TITLE Build and push a docker image.
 
-docker push petermcd/python_autorun:0.0.1
-docker push petermcd/python_autorun:latest
+:: Fetch details from the user.
+set /p version=What would you like to set the version too?
+
+:: Create the buildx environment.
+ECHO Building the dockerx environment.
+docker buildx create --name petermcd/python_autorun
+docker buildx use %image%
+docker buildx inspect --bootstrap
+
+:: Build and push the image.
+ECHO Creating and pushing the images.
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t petermcd/python_autorun:%version% --push . --no-cache
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t petermcd/python_autorun:latest --push .
